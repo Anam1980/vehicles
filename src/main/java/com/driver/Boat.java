a9829319692a38349420a08a37f85a0b926bc73e
Anam Sheikh <anamsheikh198@gmail.com>
2023-08-30 18:19:37 +0530

    test3

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,14 +2,29 @@ package com.driver;
 
 public class Boat implements WaterVehicle {
 
-    public  String name;
-    public  int capacity;
+    private  String name;
+    private  int capacity;
 
     public Boat(String name, int capacity){
         this.name = name;
         this.capacity = capacity;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
     @Override
     public String getVehicleName() {
